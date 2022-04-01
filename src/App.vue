053b680e70d1d0f9cc65255fc5089b577f047406
wuj <1450626722@qq.com>
2022-04-01 17:53:29 +0800

    ✨ feat(): Popper

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
   <div>{{ store.countPow2 }}</div>
   <div>{{ store.otherStoreCount }}</div>
   <button @click="store.countPlusOne">countPlusOne</button>
+  <Popper>
+    <button>Trigger element</button>
+    <template #content>
+      <div>This is the Popper content</div>
+    </template>
+  </Popper>
 </template>
 
 <script setup lang="ts">
@@ -13,6 +19,8 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from "./components/HelloWorld.vue";
 import { useStore } from "./store/index";
+import Popper from "vue3-popper";
+
 const store = useStore();
 console.log(store);
 
@@ -32,5 +40,16 @@ console.log(store);
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+:root {
+  --popper-theme-background-color: #333333;
+  --popper-theme-background-color-hover: #333333;
+  --popper-theme-text-color: #ffffff;
+  --popper-theme-border-width: 0px;
+  --popper-theme-border-style: solid;
+  --popper-theme-border-radius: 6px;
+  --popper-theme-padding: 32px;
+  --popper-theme-box-shadow: 0 6px 30px -6px rgba(0, 0, 0, 0.25);
 }
 </style>
